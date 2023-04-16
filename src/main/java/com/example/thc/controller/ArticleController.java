785a3946b2354fe4b537ac6200bcc135962d433d
zhuangjinxiariwanfen <hello@heima.cn>
2023-04-16 23:16:18 +0800

    完善功能

--- src/main/java/com/example/thc/controller/ArticleController.java
+++ src/main/java/com/example/thc/controller/ArticleController.java
@@ -30,6 +30,8 @@ public class ArticleController {
     private CommentService commentService;
 
     @Autowired
+    private CollectService collectService;
+    @Autowired
     private CommentLikesService commentLikesService;
     @GetMapping("/article")
     public Article getArticle(@RequestParam("id") Long id) {
@@ -230,14 +232,46 @@ public class ArticleController {
     }
 
     @PostMapping("/collect")
-    public boolean collect(@RequestBody(required = false) String id) {
-        if (id != null) {
-            // 根据文章id设置为已收藏
-            // 省略具体实现
-            return true;
-        } else {
+    public boolean collect(@RequestBody(required = false) Collect collect) {
+        if (collect.getUserId() != null) {
+//            判断当前用户是否已经收藏，如果收藏就取消收藏。如果没有收藏就进行收藏
+            LambdaQueryWrapper<Collect> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(Collect::getArticleId,collect.getArticleId());
+            queryWrapper.eq(Collect::getUserId,collect.getUserId());
+            Collect collectServiceOne = collectService.getOne(queryWrapper);
+            if(collectServiceOne==null){
+               log.info("表示当前用户没有收藏,根据文章id设置为已收藏");
+                collectService.save(collect);
+                return true;
+            }else{
+                log.info("表示用户已经收藏了，取消用户收藏");
+                collectService.removeById(collectServiceOne);
+                return false;
+            }
+
+        } else if(collect.getUserId()==null){
+//            表示用户没有登录
+            return false;
+        }else{
+            log.info("未知情况，一切返回false");
             return false;
         }
     }
-
+    @GetMapping("/getCollect")
+    public boolean getCollect(@RequestParam Long articleId,@RequestParam Long userId){
+        Collect collect = new Collect();
+        collect.setArticleId(articleId);
+        collect.setUserId(userId);
+        LambdaQueryWrapper<Collect> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Collect::getArticleId,collect.getArticleId());
+        queryWrapper.eq(Collect::getUserId,collect.getUserId());
+        Collect collectServiceOne = collectService.getOne(queryWrapper);
+        if(collectServiceOne==null){
+            log.info("表示当前用户没有收藏");
+            return false;
+        }else{
+            log.info("表示用户已经收藏了");
+            return true;
+        }
+    }
 }
